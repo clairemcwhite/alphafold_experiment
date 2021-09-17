eb0f9c3d29f4aa2e8b404caf024582646789ea97
Claire D. McWhite <cmcwhite@della-gpu.princeton.edu>
2021-09-17 11:20:01 -0400

    Needs full path

--- make_sbatch.sh
+++ make_sbatch.sh
@@ -1,10 +1,9 @@
 FASTA_LIST=$1
 OUTPUT_DIR=$( pwd )/output_alphafold
-
-
 mkdir $OUTPUT_DIR
 
 while read f
    do
-      OUTFILE=run_alphafold_${f##*/}.sbatch; sed "s@FASTAFILE@$f@" run_alphafold_TEMPLATE.sbatch | sed "s@OUTPUTDIR@$OUTPUT_DIR@" | sed "s@JOBNAME@${f##*/}@" > $OUTFILE
+      fp=$( pwd )/$f
+      OUTFILE=run_alphafold_${f##*/}.sbatch; sed "s@FASTAFILE@$fp@" run_alphafold_TEMPLATE.sbatch | sed "s@OUTPUTDIR@$OUTPUT_DIR@" | sed "s@JOBNAME@${f##*/}@" > $OUTFILE
    done < $FASTA_LIST  
