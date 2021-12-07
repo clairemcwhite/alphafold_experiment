20e941f3a7a23199af98ff3c2edb90a96d371032
Claire D. McWhite <cmcwhite@della5.princeton.edu>
2021-12-07 15:12:28 -0500

    Add scripts for running della alphafold installation

---
+++ make_sbatch_alphafold2.1.sh
@@ -0,0 +1,10 @@
+FASTA_LIST=$1
+OUTPUT_DIR=$( pwd )/output_alphafold
+mkdir $OUTPUT_DIR
+
+while read f
+   do
+      fp=$( pwd )/$f
+      protname=${f##*/}
+      OUTFILE=run_alphafold_${protname}.sbatch; sed "s@FASTAFILE@$fp@" run_alphafold2.1_TEMPLATE.sbatch | sed "s@OUTPUTDIR@$OUTPUT_DIR@" | sed "s@JOBNAME@$protname@" > $OUTFILE
+   done < $FASTA_LIST  
