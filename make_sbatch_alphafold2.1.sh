aeba14c1de9e4720e40c0366d6df766f53d92a55
Claire D. McWhite <cmcwhite@della-gpu.princeton.edu>
2021-12-07 15:32:17 -0500

    Fix path change

--- make_sbatch_alphafold2.1.sh
+++ make_sbatch_alphafold2.1.sh
@@ -4,7 +4,7 @@ mkdir $OUTPUT_DIR
 
 while read f
    do
-      fp=$( pwd )/$f
+      fp=$f
       protname=${f##*/}
       OUTFILE=run_alphafold_${protname}.sbatch; sed "s@FASTAFILE@$fp@" run_alphafold2.1_TEMPLATE.sbatch | sed "s@OUTPUTDIR@$OUTPUT_DIR@" | sed "s@JOBNAME@$protname@" > $OUTFILE
    done < $FASTA_LIST  
